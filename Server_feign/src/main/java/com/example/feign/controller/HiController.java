5d9e2d8ae6cf1b35c929cadc21f15a810775d55e
wuguangyao <674147537@qq.com>
2018-04-20 09:34:29 +0800

    1.加入Feign 2.Eureka定时清除已经关闭服务的配置

---
+++ Server_feign/src/main/java/com/example/feign/controller/HiController.java
@@ -0,0 +1,20 @@
+package com.example.feign.controller;
+
+import com.example.feign.service.HiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    @Autowired
+    HiService hiService;
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return hiService.sayHiFromClientOne(name);
+    }
+}
