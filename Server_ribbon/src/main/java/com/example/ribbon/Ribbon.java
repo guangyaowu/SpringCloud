edb02cb5cf51fce4d3db9f6abbf3ecedd73abc11
wuguangyao <674147537@qq.com>
2018-04-20 17:02:07 +0800

    HystrixDashboard调试成功

--- Server_ribbon/src/main/java/com/example/ribbon/Ribbon.java
+++ Server_ribbon/src/main/java/com/example/ribbon/Ribbon.java
@@ -1,7 +1,10 @@
 package com.example.ribbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -12,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableCircuitBreaker
 @EnableHystrixDashboard
 public class Ribbon {
 
@@ -24,6 +28,17 @@ public class Ribbon {
 	@LoadBalanced//@LoadBalanced注解表明这个restRemplate开启负载均衡的功能
 	RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+
+	@Bean//HystrixDashboard监控需要的servlet，没有自动注册，需要手动注入
+	ServletRegistrationBean getServlet(){
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(streamServlet);
+		servletRegistrationBean.setLoadOnStartup(1);
+		servletRegistrationBean.addUrlMappings("/hystrix.stream");
+		servletRegistrationBean.setName("HystrixMetricsStreamServlet");
+		return servletRegistrationBean;
 	}
 
 }
